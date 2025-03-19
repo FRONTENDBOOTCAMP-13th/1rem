185d080274bf460dedba78671653da0f9d0bd098
YouVin <dbqls990909@naver.com>
2025-03-19 21:38:09 +0900

    [feat] 황유빈 / main components 완료, header component 제작, footer component 제작

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-import "./style.css"
+import './style.css';
+import { includeHtml } from './js/header.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // 모든 includeJs 요소 초기화
+  includeHtml();
+});
